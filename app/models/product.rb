6196417017ad329dabced35463707c533c2c31ef
Przemyslaw Falowski <przemkow92@gmail.com>
2015-02-04 02:33:37 +0100

    solve product_spec issues

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  #Validations 
+  validates_presence_of :title, :price, :description
+  validates :price ,format: {with: /\A\d+(?:\.\d{0,2})?\z/} #maybe gem money-rails,would make it more clear(?)
+
+  def average_rating
+  	self.reviews.average(:rating) # .average!!! -method to REMEMBER! 
+  end
+
 end
