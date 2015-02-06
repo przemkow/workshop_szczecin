9f01103dc5b9097da6a6a6b30087f3f50b6af9a4
Przemyslaw Falowski <przemkow92@gmail.com>
2015-02-06 01:52:11 +0100

    fixed problem with saving review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,8 +7,8 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    review = Review.new(review_params)
-    review.user_id = current_user
+    self.review = Review.new(review_params)
+    review.user = current_user
     if review.save
       product.reviews << review
       redirect_to category_product_url(product.category, product), notice: 'Review was successfully created.'
